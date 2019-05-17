c845b8f48adf8c4b2924a1620385a59c211e9d7b
Hadi Ziadeh <hadiziadeh@hotmail.com>
2019-05-17 21:48:39 +0300

    FIXXXXX

--- FinalGameProject/hero.js
+++ FinalGameProject/hero.js
@@ -1,3 +1,5 @@
+var deletearray1 = [];
+var start2 = true;
 class Hero {
   constructor(x, y, ctx) {
     this.x = x;
@@ -10,23 +12,22 @@ class Hero {
     this.framesCount = 0;
     this.tickCounter = 0;
     this.ticksPerFrame = 10;
-
+    this.level2 = true;
     this.idleY = 150;
     this.walkingY = 0;
-
+    this.level1 = false;
     this.ground = y;
-
+    this.shoot = 0;
     this.jumpHeight = this.ground - 220;
     this.jumpState = 0;
     this.jumpSpeed = 800;
-
     this.idle = true;
     this.landed = true;
-
     this.fire = true;
     this.bulletHotSpotX;
     this.bulletHotSpotY;
     this.bullet = [];
+    this.sprites1 = [];
     var _this = this;
     setInterval(function() {
       _this.fire = true;
@@ -95,15 +96,28 @@ class Hero {
     }
     ////////////////////////////////////////////////////////////////////////////////
     //Bullet
-
-    if (90 in keysDown && this.fire == true) {
-      this.bulletHotSpotX = this.x + 95;
-      this.bulletHotSpotY = this.y + 30;
-      this.bullet.push(
-        new HeroBullet(this.bulletHotSpotX, this.bulletHotSpotY, this.ctx)
-      );
-      this.fire = false;
+    if (this.level1 == true) {
+      if (90 in keysDown && this.fire == true) {
+        this.bulletHotSpotX = this.x + 95;
+        this.bulletHotSpotY = this.y + 30;
+        this.bullet.push(
+          new HeroBullet(this.bulletHotSpotX, this.bulletHotSpotY, this.ctx)
+        );
+        this.fire = false;
+      }
     }
+    if (this.level2 == true) {
+      if (90 in keysDown) {
+        if (start2 == true) {
+          this.shoot = new Laser(this.x, this.y, "level2", this.ctx);
+          this.sprites1.push(this.shoot);
+          delete keysDown[90];
+          this.shoot.start = false;
+          start2 = false;
+        }
+      }
+    }
+
     // console.log(this.fire);
     /////////////////////////////////////////////////
     //JUMP
@@ -126,7 +140,84 @@ class Hero {
     }
 
     for (let i = 0; i < this.bullet.length; i++) {
-      this.bullet[i].update();
+      this.bullet[i].update(this.x, this.y);
     }
+    for (let i = 0; i < this.sprites1.length; i++) {
+      this.sprites1[i].update(this.x, this.y);
+      if (this.sprites1[i].coolingdown == false) this.sprites1[i].draw();
+    }
+    for (var j = 0; j < deletearray1.length; j++) {
+      var indexof = this.sprites1.indexOf(deletearray1[j]);
+      this.sprites1.splice(indexof, 1);
+    }
+    //emptying the delete array
+    deletearray1 = [];
   };
+}
+class Laser {
+  x = 0;
+  y = 0;
+  width = 0;
+  name = " ";
+  laserImage = new Image();
+  laserWidth = 0;
+  lasertime = 0;
+  coolDown = 0;
+  collide = false;
+  coolingdown = false;
+  constructor(x, y, name, ctx) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.ctx = ctx;
+  }
+  draw() {
+    this.laserImage.src = "./laser.png";
+    this.ctx.drawImage(
+      this.laserImage,
+      this.x + 100,
+      this.y + 30,
+      this.width,
+      15
+    );
+  }
+  update(x, y) {
+    if (this.collide == false) {
+      this.width += 10;
+    }
+    if (this.collide == true && this.coolingdown == true) {
+      this.width = 0;
+    }
+    this.laserWidth = this.width + this.x;
+    this.x = x;
+    this.y = y;
+    if (this.coolingdown == false) this.lasertime++;
+    if (this.coolingdown == true) this.lasertime -= 1.4;
+    ctx.fillStyle = "rgb(250, 110, 95)";
+    ctx.font = "24px Helvetica";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("BrainHeat: ", 200, 680);
+    drawrect(this.ctx, "black", 200, 700, 210, 20, "black");
+    drawrect(this.ctx, "orange", 200, 700, this.lasertime, 20, "orange");
+    if (this.lasertime == 210) {
+      this.coolingdown = true;
+    }
+    if (this.coolingdown == true) {
+      this.coolDown++;
+      if (this.coolDown == 150) {
+        start2 = true;
+        this.coolingdown == false;
+        deletearray1.push(this);
+      }
+    }
+  }
+}
+function drawrect(context, color, x, y, length, height, col2) {
+  context.beginPath();
+  context.fillStyle = color;
+  context.fillRect(x, y, length, height);
+  context.lineWidth = 4;
+  context.strokeStyle = col2;
+  context.stroke();
 }
