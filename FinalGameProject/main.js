ce370c2bdbf2190a6fbc84d95d92fb5717621d1f
samir maroun <samirmaroun1999@hotmail.com>
2019-05-25 13:19:56 +0300

    menuBar

--- FinalGameProject/main.js
+++ FinalGameProject/main.js
@@ -42,26 +42,55 @@ let level2 = new Level2(ctx, canvas, keysDown);
 
 let level = 1;
 audio.volume = 0.4;
+
+var endgame = false;
 let main = () => {
-  if (level2.GameOver != true && level1.GameOver != true) {
-    audio.play();
-  } else {
-    gameOverAudio.play();
-    audio.pause();
-  }
-  if (level == 1) {
-    level1.main();
-    if (level1.skeletonBoss.healthBar < 0) {
-      if (showCounter > 0) {
-        ctx.drawImage(level1Victory, 0, 0, canvas.width, canvas.height);
-        showCounter--;
-      } else {
-        level = 2;
+  //CHeCKING If player chose his option
+  if (chosen == false) {
+    ctx.drawImage(backgroundmenu, 0, 0, canvas.width, canvas.height);
+
+    //taking input from the mouse
+    addEventListener("click", options, false);
+    //assigning option of user
+    function options(ev) {
+      var x = ev.clientX - canvas.offsetLeft;
+      var y = ev.clientY - canvas.offsetTop;
+      console.log("x:" + x);
+      console.log("y:" + y);
+      //starting game
+      if (x >= 28 && x < 248 && y >= 81 && y < 263) {
+        chosen = true;
+      }
+      //ending game
+      else if (x >= 48 && x < 256 && y >= 526 && y < 690) {
+        endgame = true;
       }
     }
-  } else {
-    level1.stop();
-    level2.main();
+  }
+  if (chosen == true && endgame == false) {
+    if (level2.GameOver != true && level1.GameOver != true) {
+      audio.play();
+    } else {
+      gameOverAudio.play();
+      audio.pause();
+    }
+    if (level == 1) {
+      level1.main();
+      if (level1.skeletonBoss.healthBar < 0) {
+        if (showCounter > 0) {
+          ctx.drawImage(level1Victory, 0, 0, canvas.width, canvas.height);
+          showCounter--;
+        } else {
+          level = 2;
+        }
+      }
+    } else {
+      level1.stop();
+      level2.main();
+    }
+  }
+  if (endgame == true) {
+    ctx.drawImage(exitBackground, 0, 0, canvas.width, canvas.height);
   }
   requestAnimationFrame(main);
 };
